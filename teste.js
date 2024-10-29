efbed156c0e82412e36a14682a70c909a8fe1364
Diogo silva <d.diogosila@gmail.com>
2024-10-29 16:47:30 -0300

    commit up
    atualizaçao nas telas

--- teste.js
+++
@@ -1,4 +0,0 @@
-document.getElementById('hamburguer-btn').addEventListener('click', function() {
-    const menu = document.getElementById('menu-hamburguer');
-    menu.classList.toggle('active');
-});

a9e9e429172e470e5a62678530d0d413324b671c
Thaisa123 <163612089+Thaisa123@users.noreply.github.com>
2024-11-13 23:35:55 -0300

    commit cad.usuario
    cadastro de usuario PHP

---
+++ cad_usuario.php
@@ -0,0 +1,34 @@
+<?php
+
+$hostname = "localhost";
+$usuario = "root";
+$senha = ""; // se tiver senha, coloque aqui
+$bancodedados = "cad_usuario";
+
+
+
+// Criando conexão
+$conn = mysqli_connect($hostname, $usuario, $senha, $bancodedados);
+
+// Verificando a conexão
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+
+echo "conectado com sucesso";
+
+$nome = $_POST['nome'];
+$email = $_POST['email'];
+$senha = md5($_POST['senha']);
+$sql ="INSERT INTO pessoas (nome,email,senha) VALUES('$nome','$email','$senha')";
+if($conn->query($sql) ===TRUE) {
+    echo "usuário cadastrado!";
+    header("location:http://localhost/AdoteUmAmor-main/index.html#apadrinhamento");
+
+    exit();
+} else{
+    echo "Não foi possivel cadastrar usuário!".$conn->error;
+}
+
+$conn->close();
+?>
